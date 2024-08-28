e2bff8b8dca2db6cc5f9fb231fafa84133283dfc
stormzhangbx <13128814051@163.com>
2024-08-28 23:37:37 +0800

    fix: 修复打包失败的问题

--- .vitepress/config.ts
+++ .vitepress/config.ts
@@ -19,6 +19,7 @@ export default defineConfig({
   lastUpdated: true,
   useWebFonts: false,
   cleanUrls: true,
+  ignoreDeadLinks: true,
   markdown: {
     lineNumbers: true,
   },
--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { dirname, join, resolve } from 'node:path'
 import { defineConfig } from 'vite'
-import VueComponents from 'unplugin-vue-components/vite'
+import Components from 'unplugin-vue-components/vite'
 import { AntDesignVueResolver } from 'unplugin-vue-components/resolvers'
 import react from '@vitejs/plugin-react'
 import MarkdownTransform from './.vitepress/plugins/vite-plugin-md-transform'
@@ -10,28 +10,26 @@ export default defineConfig(() => {
     resolve: {
       alias: {
         '#': resolve(__dirname, './.vitepress'),
-        // '#/components': resolve(__dirname, './.vitepress/components'),
       },
-      // alias: [
-      //   { find: '/@/', replacement: 'aaa' },
-      // ],
     },
     optimizeDeps: {
       exclude: [
         '@nolebase/vitepress-plugin-enhanced-readabilities/client',
-        'vitepress',
       ],
     },
     ssr: {
       noExternal: [
         '@nolebase/vitepress-plugin-enhanced-readabilities',
         '@nolebase/vitepress-plugin-highlight-targeted-heading',
+        'ant-design-vue',
+        '@ant-design/icons-svg',
+        '@ant-design/icons-vue',
       ],
     },
     plugins: [
       react(),
       MarkdownTransform(),
-      VueComponents({
+      Components({
         dirs: './.vitepress/components/vue',
         include: [/\.vue$/, /\.vue\?vue/, /\.md$/],
         dts: './.vitepress/components.d.ts',
